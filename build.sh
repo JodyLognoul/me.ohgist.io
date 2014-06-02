a031d040644b5583b0e0915f7c6e3da22cfa5cc4
Jody Lognoul <lognoulj@gmail.com>
2014-06-02 09:22:16 +0800

    06-02-2014

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 git add -A
 NOW=$(date +"%m-%d-%Y")
 git commit -am $NOW
-# git push -u https://github.com/Narcotic/egam.io.git master
+git push -u https://github.com/Narcotic/me.egam.io.git master
 # cd
 # ssh -i .ssh/key2014.pem ubuntu@54.247.179.219
 # cd domains/egam.io/
