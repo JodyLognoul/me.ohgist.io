2310f5fc35c6cfd8236ecbc1297688cd2cbb7dc2
Jody Lognoul <lognoulj@gmail.com>
2014-06-06 00:09:30 +0700

    06-06-2014

--- Gruntfile.js
+++ Gruntfile.js
@@ -241,8 +241,7 @@ module.exports = function(grunt) {
                 files: [{
                     expand: true,
                     cwd: '<%= config.app %>/images',
-                    src: '{,*/}*.jpg',
-                    // src: '{,*/}*.{gif,jpeg,jpg,png}',
+                    src: '{,*/}*.{gif,jpeg,jpg,png}',
                     dest: '<%= config.dist %>/images'
                 }]
             }
